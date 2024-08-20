e6af830e6f2d43148d4cf424eed4d3c328c66659
kolserdav <serega12101983@gmail.com>
2024-08-20 17:36:30 +0700

    initial

---
+++ hello-world/index.js
@@ -0,0 +1,18 @@
+// @ts-check
+import http from 'http';
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  console.info('Request', req.headers);
+  res.end(
+    JSON.stringify({
+      data: 'Hello World!',
+    })
+  );
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log('Server is listenning at port ', port);
+});
