f6f1a7c38ae3e683253ec14c85257238307a8850
Sergei Kolmiller <serega12101983@gmail.com>
2024-08-22 17:20:29 +0700

    Update index.js

--- hello-world/index.js
+++ hello-world/index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   console.info('Request', req.headers);
   res.end(
     JSON.stringify({
-      data: 'Hello Test',
+      data: 'Hello Test 1',
     })
   );
 });
